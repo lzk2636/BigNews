04f2ef0904aa146e4517e6722240a8405d0eae48
lzk2636 <2820421644@qq.com>
2019-11-16 17:31:01 +0800

    http更新

--- admin/libs/http.js
+++ admin/libs/http.js
@@ -20,6 +20,10 @@
         comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
         comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
         comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
+        data_info:baseURL+'/admin/data/info',//获取统计数据
+        data_article:baseURL+'/admin/data/article',//日新增文章数量统计
+        data_category:baseURL+'/admin/data/category',//日新增文章数量统计
+        data_visit:baseURL+'/admin/data/visit',//日文章访问量
     };
 
     //暴露接口
